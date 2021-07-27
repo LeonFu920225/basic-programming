803883780b7746cc4034fedbdffc70eab7e34442
LeonFu920225 <87316116+LeonFu920225@users.noreply.github.com>
2021-07-27 18:42:20 +0800

    Delete 階乘計算.py

--- 階乘計算.py
+++
@@ -1,9 +0,0 @@
-def factorial(n):
-    if n == 1:
-        return 1
-    elif n != int:
-        print("Error!")
-    else:
-        return (n*factorial(n-1))
-N = eval(input("Please enter a number:"))
-print(N, "'s factorial result = ", factorial(N))
